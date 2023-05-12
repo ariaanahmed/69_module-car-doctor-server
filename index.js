3cfb36637de18cede7b0de71fa9cf6f875aa87e5
ariaan ahmed <you@example.com>
2023-05-12 19:43:55 +0600

    added

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ async function run() {
       res.send(result);
     })
 
-
     app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
